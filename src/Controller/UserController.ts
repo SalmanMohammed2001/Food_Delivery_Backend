e9ddee3cd039cc514ded402122b339e5ac1cc0ed
salman <salmannisthar123@gmail.com>
2024-06-11 09:24:04 +0530

    implementation user login

--- src/Controller/UserController.ts
+++ src/Controller/UserController.ts
@@ -11,39 +11,28 @@ const validator=require('validator');
 
 const loginUser = async (req: any, res: any) => {
 
-    // try {
-    //     const {name, email, password} = req.body
-    //
-    //     const exists = await UserModel.findOne({email})
-    //
-    //     if (exists) {
-    //         return res.status(500).json({success: false, message: "User already exists"})
-    //     }
-    //
-    //     if(!validator.isEmail(email)){
-    //         return res.status(500).json({success: false, message: "please enter valid email"})
-    //     }
-    //
-    //     if(password.length<8){
-    //         return res.status(500).json({success: false, message: "please enter strong password"})
-    //     }
-    //
-    //     const salt=await  bcrypt.genSalt(10);
-    //     const hashPassword=await bcrypt.hash(password,salt)
-    //
-    //     const newUser =new UserModel({
-    //         name:name,
-    //         email:email,
-    //         password:hashPassword
-    //     })
-    //
-    //   const user =await  newUser.save()
-    //
-    //
-    //     return res.status(200).json({success: true, message: "food Successfully created"})
-    // } catch (error) {
-    //     return res.status(500).json({success: false, message: "Internal server error"})
-    // }
+    try {
+        const { email,password} = req.body
+
+        const user = await UserModel.findOne({email})
+
+        if (!user) {
+            return res.status(500).json({success: false, message: "User not found"})
+        }
+
+        const isMatch=await  bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.status(500).json({success: false, message: "Invalid credentials"})
+        }
+
+        const token=createToken(user._id);
+
+
+        return res.status(200).json({success: true, message: "login Successfully ",token})
+    } catch (error) {
+        return res.status(500).json({success: false, message: "Internal server error"})
+    }
 
 }
 
@@ -90,7 +79,6 @@ const registerUser = async (req: any, res: any) => {
     } catch (error) {
         return res.status(500).json({success: false, message: "Internal server error"})
     }
-
 
 }
 
